f990a39195a588f62aaffba968f875a3a678ab64
Ming Ying <yingming18@gmail.com>
2023-11-28 20:11:20 -0500

    Added first parameter

--- hello.py
+++ hello.py
@@ -2,9 +2,12 @@ from neo4j import Driver
 from prefect import flow, task
 
 @task
-def print_hello():
-    print("Hello, Prefect!")
+def print_hello(who):
+    print(f"Hello, {who}!")
 
 @flow(name="Hello", log_prints=True)
-def first_flow():
-    print_hello()
+def first_flow(who):
+    print_hello(who)
+
+if __name__ == "__main__":
+    first_flow("Pure Python")
